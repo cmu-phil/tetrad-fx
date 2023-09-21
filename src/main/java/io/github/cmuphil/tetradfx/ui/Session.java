0a73640f64207da3baea34bc1407d78112834000
jdramsey <jdramsey@andrew.cmu.edu>
2023-09-20 23:47:50 -0400

    Added method to delete projects from a session in the File menu, with stern warning.

--- src/main/java/io/github/cmuphil/tetradfx/ui/Session.java
+++ src/main/java/io/github/cmuphil/tetradfx/ui/Session.java
@@ -182,8 +182,6 @@ public class Session {
             getSelectedProject().setParametersAndNotesText();
         }
 
-        System.out.println("dir: " + dir.getAbsolutePath());
-
         sessionTreeView.setOnMouseClicked(event -> {
             if (event.getClickCount() == 2) {
                 var selectedItem = sessionTreeView.getSelectionModel().getSelectedItem();
